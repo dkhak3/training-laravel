a42f74c47a30ac6d1cb7d0b091959a33838673cf
KHA  DUY <nhokkha345@gmail.com>
2023-05-02 12:45:15 +0700

    update

--- be2/resources/views/dashboard.blade.php
+++ be2/resources/views/dashboard.blade.php
@@ -1,91 +1,53 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Laravel 8 Custom Login And Registration Example - NiceSnippets.com</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            .pagination-page{
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>
+    </head>
 
-<head>
-    <title>Laravel 8 Custom Login And Registration Example - NiceSnippets.com</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .pagination-page{
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-    </style>
-</head>
-
-<body>
-    <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
-        <div class="container">
-            <a class="navbar-brand mr-auto" href="#">NiceSnippets</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register-user') }}">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('listuser') }}">Admin</a>
-                        </li>
-                    @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('signout') }}">Logout</a>
-                        </li>
-                        @isset($id)
+    <body>
+        <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+            <div class="container">
+                <a class="navbar-brand mr-auto" href="#">NiceSnippets</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('viewUser', $id) }}">View user</a>
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
                             </li>
-                        @endisset
-                    @endguest
-                </ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('listuser') }}">Admin</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                            </li>
+                            @isset($id)
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('viewUser', $id) }}">View user</a>
+                                </li>
+                            @endisset
+                        @endguest
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-
-    <!-- <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
-    </div> -->
-    @yield('content')
-</body>
+        </nav>
+        @yield('content')
+    </body>
 
 </html>
